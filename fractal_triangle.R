2de20fd5d39da2fe68a7791b1ea6fcf7f98b598c
matiaspoullain <58349277+matiaspoullain@users.noreply.github.com>
2022-07-10 23:41:06 -0300

    emrpolijamiento de la imagen

--- fractal_triangle.R
+++ fractal_triangle.R
@@ -1,38 +1,49 @@
 rm(list = ls())
 gc()
 
-library(tidyverse)
+library(dplyr)
+library(ggplot2)
 library(data.table)
 
-#Funciones:
+#Número de puntos a graficar:
+#Number of point to plot
+repeticiones <- 500000
 
+#Crear un triangulo equilatero al azar
+#Create a equilateral random triangle
 
-#Creo un triangulo equilatero al azar
-##Primero dos puntos al al azar:
+##Primero, dos puntos al azar, entre 0 y 1:
+##First, two random points, between 0 and 1: 
 
 vertices <- matrix(runif(4), ncol = 2)
 
-##Calculo la distancia entre amvos
+##Calcular la distancia entre ambos:
+##Get distance between them:
 distancia <- dist(vertices)[1]
 
 
-#Tercer vertice que construya el triangulo equilatero
-
-tercer.vertice <- 0.5 * colSums(vertices) + sqrt((distancia**2/(diff(vertices[,1])**2 + diff(vertices[,2])**2))-0.25) * c(diff(vertices[,2]), -diff(vertices[,1]))
+#Tercer vértice que construya el triangulo equilatero
+##Dados los dos primeros puntos, existen dos posibilidades para el tercer punto. Este se elije al azar
+#Third vertex to complete equilateral triangle
+##Given the first two vertexes, there are two posibilities for the third one. It's randomly selected
+tercer.vertice <- 0.5 * colSums(vertices) + sample(c(1, -1), 1) *  sqrt((distancia**2/(diff(vertices[,1])**2 + diff(vertices[,2])**2))-0.25) * c(diff(vertices[,2]), -diff(vertices[,1]))
 
 vertices <- rbind(vertices, tercer.vertice)
 
 #Verifico que sea un triangulo  equilatero:
-dist(vertices) 
+#Checking that the triangle is equilateral
+dist(vertices)
 
-# Genero al azar un punto dentro del triangulo:
+
+#Generar al azar un punto dentro del triangulo:
+#Randomly create a point inside the triangle:
+#Source: https://www.cs.princeton.edu/~funk/tog02.pdf
 r <- runif(2)
 punto.inicial <- (1-sqrt(r[1]))*vertices[1,] + sqrt(r[1]) * (1 - r[2]) * vertices[2,] + (r[2]*sqrt(r[1]))*vertices[3, ]
 
 
-#Busco el punto intermedio entre el punto al azar y un vertice al azar:
-
-repeticiones <- 10000
+#Encuentro el punto intermedio entre el punto al azar y un vertice al azar. El proceso se repite las veces indicada:
+#Finding the middle point between the random point and one of the vertexes, randomly selected. Repeat the process as many times as it was commanded:
 
 puntos.x <- vector("numeric", repeticiones)
 puntos.y <- puntos.x
@@ -45,17 +56,18 @@ for(i in 1:repeticiones){
   puntos.y[i] <- punto.inicial[2]
 }
 
-puntos.nuevos <- data.table(V1 = puntos.x, V2 = puntos.y)
+puntos.nuevos <- data.table(V1 = puntos.x, V2 = puntos.y) %>%
+  rbind(vertices)
 
+#Graficar los puntos en un lienzo vacío:
+#Plot the points in a empty canvas:
 
-puntos.nuevos %>%
+plot.triangulos <-  puntos.nuevos %>%
   ggplot(aes(x = V1, y = V2)) +
-  geom_point()
+  geom_point(colour = "#E27A06", size = 0.01, shape = 17) +
+  theme_void() +
+  coord_equal()
 
-
-vertices %>%
-  as.data.frame() %>%
-  ggplot(aes(x = V1, y = V2)) +
-  geom_point(size = 0.1) +
-  geom_polygon(col = "black", fill = "transparent") +
-  geom_point(data = puntos.nuevos, colour = "blue")
+#Guardado como imagne
+#Save as image
+ggsave("fractal_triangle.png", plot.triangulos, width = 25, height = 25, dpi = 800)
